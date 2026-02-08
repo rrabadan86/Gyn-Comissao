8c2b7eb263ec60ff539b2e75a9822fdfd5d2b8ac
rrabadan86 <rodrigorabadan@gmail.com>
2026-02-08 13:52:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def extrair_tabela(driver, tabela_element):
     lista = [c.text.strip() for c in celulas if c.text.strip() != ""]
     html = """<table style="border-collapse: collapse; width: 600px; font-family: Arial; border: 1px solid #ddd;">
     <tr style="background-color: #0f4c3a; color: white;"><th style="padding: 10px;">Vendedor</th><th style="padding: 10px;">Comissão</th><th style="padding: 10px;">Prêmiação</th></tr>"""
-    blacklist = ["Meta", "Bonus", "TKM", "PMA", "Tot.", "Vendedor", "Comissão", "Premiação", "Prêmiação", "IPA", "Gorjeta"]
+    blacklist = ["Meta", "Bonus", "TKM", "PMA", "Tot.", "SEM VENDEDOR", "Vendedor", "Comissão", "Premiação", "Prêmiação", "IPA", "Gorjeta"]
     i = 0
     while i < len(lista):
         item = lista[i]
@@ -184,7 +184,7 @@ def extrair_tabela_gorjeta(driver, tabela_element):
     <tr style="background-color: #0f4c3a; color: white;"><th style="padding: 10px;">Vendedor</th><th style="padding: 10px;">Gorjeta</th></tr>"""
     
     # BLACKLIST REFORÇADA PARA IGNORAR LIXO DA TABELA GRANDE
-    blacklist = ["Vendedor", "Gorjeta", "R$ Total", "TKM", "PMA", "IPA", "Meta", "Bonus", "Premiação", "Comissão", "Tot.", "Arom.", "Puro", "Acess."]
+    blacklist = ["SEM VENDEDOR", "Vendedor", "Gorjeta", "R$ Total", "TKM", "PMA", "IPA", "Meta", "Bonus", "Premiação", "Comissão", "Tot.", "Arom.", "Puro", "Acess."]
     
     i = 0
     while i < len(lista):
@@ -305,4 +305,5 @@ if __name__ == "__main__":
         a, m, y, h_comissao, h_gorjeta, meta = executar_robo()
         enviar_email(a, m, y, h_comissao, h_gorjeta, meta)
     except Exception as e: print(f"Erro: {e}")
+
 
